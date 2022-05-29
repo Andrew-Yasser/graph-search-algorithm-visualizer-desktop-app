8cd2d9108af02c0aefbaa5a6763b0663b2c1f988
Andrew-Yasser <49915521+Andrew-Yasser@users.noreply.github.com>
2022-05-29 14:54:05 +0200

    Create Breadth first.py

---
+++ Breadth first.py
@@ -0,0 +1,56 @@
+graph = { "a" : ["b","c"],
+          "b" : ["e", "f"],
+          "c" : ["G", "H"],
+          "e" : ["i"],
+          "f" : ["j"],
+          "G" : ["K"],
+		  "H" : ["l"],
+		  "i" : [],
+		  "j" : [],
+		  "K" : [],
+		  "l" : [],
+        }
+inputedges= [('a', 'b'), ('a', 'c'), ('b', 'e'), ('b', 'f'), ('c', 'G'), ('c', 'H'), ('e', 'i'), ('f', 'j'), ('G', 'K'), ('H', 'l')]
+
+edges_weights=[1,6,9,6,9,2,7,3,4,10]
+
+
+def BFS(graph, InitialNode, GoalNode):
+    visited = []
+    visited.append(InitialNode)
+    # maintain a queue of paths
+    queue = []
+    # push the first node into the queue
+    queue.append([InitialNode])
+    while queue:
+        # get the first path from the queue of paths
+        path = queue.pop(0)
+        # get the last node from the path
+        node = path[-1]
+        # if a path found to have a goal node, return it with the visited list
+        if node in GoalNode:
+            return path, visited
+        # enumerate all adjacent nodes, construct a new path and push it into the queue
+        for adjacent in graph.get(node, []):
+            if (visited[-1] not in GoalNode):
+                visited.append(adjacent)
+            new_path = list(path)
+            new_path.append(adjacent)
+            queue.append(new_path)
+            
+def CalcPathCost(inputedges):
+    weights= dict(zip(inputedges, edges_weights))
+    print(weights)
+    cost=0
+    for i in range(len(solpath)-1):
+        cost+= weights.get((solpath[i],solpath[i+1]))
+    return cost
+    path = []
+            
+            
+solpath, visitednodes = bfs(graph, 'a', ['j','G'])
+
+
+print("the solution path is", solpath)
+print("the visited nodes are",visitednodes)
+print("cost is", CalcPathCost(inputedges))
